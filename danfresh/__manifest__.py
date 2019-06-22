f5b96317c870814458d953cb98ee3f4756af0164
rkhalil1990 <rkhalil1990@yahoo.com>
2019-06-22 23:19:48 +0200

    local changes

--- danfresh/__manifest__.py
+++ danfresh/__manifest__.py
@@ -15,9 +15,7 @@
     'author': "Odoo Mates",
     'website': "http://www.yourcompany.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    
     'category': 'hr',
     'version': '1.0.0',
 

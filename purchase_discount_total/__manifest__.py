9e602f3d781a23f7858957bc3c539c057c3808cf
marioroshdy <marioroshdyn@gmail.com>
2019-10-07 18:15:13 +0200

    Purchase Discount

---
+++ purchase_discount_total/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Purchase Discount on Total Amount',
+    'version': '12.0.1.1.0',
+    'category': 'Purchase Management',
+    'summary': "Discount on Total in Purchase and Invoice With Discount Limit and Approval",
+    'author': '',
+    'company': '',
+    'website': '',
+    'description': """
+
+Purchase Discount for Total Amount
+=======================
+Module to manage discount on total amount in Purchase
+        as an specific amount or percentage
+""",
+    'depends': ['purchase',
+                'account'
+                ],
+    'data': [
+        'views/purchase_view.xml',
+    ],
+    'demo': [
+    ],
+    'images': [''],
+    'license': 'AGPL-3',
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}

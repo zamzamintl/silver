0dd1781f70910d65ba4960327da181af075bbdfd
odoo <root@ip-172-31-6-106.eu-central-1.compute.internal>
2019-11-18 12:32:04 +0000

    mario

--- addons/sale_update/models/__init__.py
+++ addons/sale_update/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from . import crm_lead
+#from . import crm_lead
 from . import sale_order
-from . import res_partner
 from . import product
-from . import opportunity_type
+#from . import opportunity_type
--- hdl-addons/hdl_sale_update/models/__init__.py
+++ hdl-addons/hdl_sale_update/models/__init__.py
@@ -2,6 +2,6 @@
 
 from . import crm_lead
 from . import sale_order
-from . import res_partner
-from . import product
+#from . import res_partner
+#from . import product
 from . import opportunity_type

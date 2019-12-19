5f33fd57a262bc54b0b7f99831194c05fd065587
Ayah <ayah@plementus.com>
2019-12-19 17:03:00 +0200

    [Add]pl_hdl:create website module and website pages odoo13

---
+++ hdl-addons/pl_hdl/static/src/js/scripts.js
@@ -0,0 +1,17 @@
+    var headerHeight = $('.top-header').outerHeight();
+    $(window).on("scroll", function() {
+    if($(window).scrollTop() > headerHeight) {
+        $("header").addClass("sticky");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $("header").removeClass("sticky");
+    }
+});
+    $(window).on("scroll", function() {
+    if($(window).scrollTop() > 500) {
+        $(".scroll_top_btn").addClass("display_btn");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".scroll_top_btn").removeClass("display_btn");
+    }
+});

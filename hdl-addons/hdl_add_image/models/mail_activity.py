86fb5185af9ca4de32d5b3dde094ff61ae020a24
khaledsalah <khaled@plementus.com>
2020-01-14 11:04:16 -0500

    remove date time from activity

--- hdl-addons/hdl_add_image/models/mail_activity.py
+++ hdl-addons/hdl_add_image/models/mail_activity.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api
 class hdl_add_mail_activity(models.Model):
     _inherit = 'mail.activity'
 
-    datetime_deadline = fields.Datetime('Due Date', index=True, required=True)
+    # datetime_deadline = fields.Datetime('Due Date', index=True, required=True)

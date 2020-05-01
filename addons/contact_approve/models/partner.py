3fb751acfeb8784ba6cfa683d2e272f31f81b8ef
mohamedahmed <mohamed.abdelrahman@businessborderlines.com>
2020-05-01 23:17:08 +0200

    contact approve

---
+++ addons/contact_approve/models/partner.py
@@ -0,0 +1,22 @@
+from odoo import api, fields ,models
+from odoo.exceptions import ValidationError 
+import logging
+_logger = logging.getLogger(__name__)
+
+class ResPartner(models.Model):
+    _inherit="res.partner"
+    state=fields.Selection([('Draft','Draft'),('Approve','Approve')],default="Draft",string="Status")
+    def action_approve(self):
+        self.state='Approve'
+class sales_order(models.Model):
+    _inherit='sale.order'
+    @api.onchange('partner_id')
+    def change_partner(self):
+        _logger.info("CHANGE PARTNER")
+        return {'domain':{'partner_id':[('state','=','Approve')]}}
+class purchase_order(models.Model):
+    _inherit='purchase.order'
+    @api.onchange('partner_id')
+    def change_partner(self):
+        _logger.info("CHANGE PARTNER")
+        return {'domain':{'partner_id':[('state','=','Approve')]}}

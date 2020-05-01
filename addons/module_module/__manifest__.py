a47413f21127c39ae9a6ee1ef853519bd645421b
mohamedahmed <mohamed.abdelrahman@businessborderlines.com>
2020-05-01 02:13:49 +0200

    fix reselller

---
+++ addons/module_module/__manifest__.py
@@ -0,0 +1,15 @@
+
+{
+    'name': 'Module module Quantity',
+    'version': '1.3',
+    'category': 'sale',
+    'description': """
+Sum quantity of some of order
+===================================================
+""",
+    'depends': ['sale','base'],
+    'data': ['views/menu.xml','views/action.xml'],
+     
+    'installable': True,
+    'auto_install': True,
+}

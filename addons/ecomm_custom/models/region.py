d5bd2a6fd21728054462ee06e9426cc96f51820d
mohamedahmed <mohamed.abdelrahman@businessborderlines.com>
2020-05-04 02:19:12 +0200

    zone edit menu

--- addons/ecomm_custom/models/region.py
+++ addons/ecomm_custom/models/region.py
@@ -11,19 +11,19 @@ class sregion(models.Model):
     _name="state.region1"
     state_id=fields.Many2one('res.country.state',string="City")
     region2=fields.One2many('state.region2','region1',string='District')
-    name=fields.Char("Name of Region 1")
+    name=fields.Char("Area")
 class state(models.Model):
     _inherit='res.country.state'
     region=fields.One2many('state.region1','state_id',string='area')
 class region2(models.Model):
     _name='state.region2'
-    name=fields.Char("Name of Region2")
+    name=fields.Char("District")
     region1=fields.Many2one('state.region1',string='Area')
-    region3=fields.One2many('state.region','region1',string='Partition')
+    region3=fields.One2many('state.region','region2',string='Partition')
 class region3(models.Model):
     _name='state.region'
-    state_id=fields.Many2one('res.country.state',string="City")
+     
     name=fields.Char("Partition")
-    region1=fields.Many2one('state.region1',string='Area',domain="[('state_id','=',state_id)]")
-    region2=fields.Many2one('state.region2',string='District',domain="[('region1','=',region1)]")
+    
+    region2=fields.Many2one('state.region2',string='District')
     

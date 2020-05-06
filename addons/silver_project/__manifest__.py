e59ef3c4d0570ec14836f8c84c7857260ed249dc
mohamedahmed <mohamed.abdelrahman@businessborderlines.com>
2020-05-07 01:20:14 +0200

    add some of module silverimportexport

---
+++ addons/silver_project/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "silver_project",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base','project'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/project_view.xml',
+    ],
+
+}

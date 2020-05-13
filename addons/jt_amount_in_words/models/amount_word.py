41ec2de311e268deafb9f605f4094ea93b63b3cd
mohamedahmed <mohamed.abdelrahman@businessborderlines.com>
2020-05-13 03:58:23 +0200

    fix bugs related between amount in word e commerace

--- addons/jt_amount_in_words/models/amount_word.py
+++ addons/jt_amount_in_words/models/amount_word.py
@@ -11,8 +11,9 @@ class SaleOrder(models.Model):
     @api.constrains("amount_total","num_word_ar")
     def _compute_amount_in_word(self):
         for rec in self:
-            rec.num_word = str(rec.currency_id.amount_to_text(rec.amount_total)) + ' only'
-            rec.num_word_ar=num2words(round(rec.amount_total,2), lang='ar')
+            if rec.amount_total:
+                rec.num_word = str(rec.currency_id.amount_to_text(rec.amount_total)) + ' only'
+                rec.num_word_ar=num2words(round(rec.amount_total,2), lang='ar')
 
     num_word = fields.Char(string="Amount In Words:", compute='_compute_amount_in_word')
     num_word_ar = fields.Char(string="Amount In Word ar", compute='_compute_amount_in_word')

ff942a33017120ea990425da46be511a85709022
Mohamed Abd EL rhman <mohamed.abdalrhman0@gmail.com>
2020-06-06 18:17:18 +0200

    create order from ticket and lead

--- addons/crm_helpdesk_custom/__manifest__.py
+++ addons/crm_helpdesk_custom/__manifest__.py
@@ -5,7 +5,17 @@
     'category': 'helpdesk',
     'description': """create sales order from ticket  and relate between ticket and lead 
 """,
-    'depends': ['crm', 'helpdesk','crm_helpdesk','sale'],
+    'depends': ['crm', 'helpdesk','crm_helpdesk','sale','base_setup',
+        'sales_team',
+        'mail',
+        'calendar',
+        'resource',
+        'fetchmail',
+        'utm',
+        'web_tour',
+        'contacts',
+        'digest',
+        'phone_validation',],
     'data': ['views/ticket_sale.xml','views/survey_sheet.xml','secuirty/ir.model.access.csv'],
      
     'installable': True,

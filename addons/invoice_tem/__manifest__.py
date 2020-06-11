18aeb16270e2ec33fc7747bd3ea657b1d7843e1b
Mohamed Abd EL rhman <mohamed.abdalrhman0@gmail.com>
2020-06-11 22:47:00 +0200

    updates

---
+++ addons/invoice_tem/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'Invoice tempelate',
+    'version' : '12.0.1',
+    'summary': 'Invoice tempelate',
+    'sequence': 16,
+    'category': 'account',
+    'author' : 'Mohamed abd Elrhman',
+    'description': """
+    """,
+    "license": "AGPL-3",
+    'depends' : [ 'account','sale'],
+    'data': ['views/templete_action.xml'],
+    
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}

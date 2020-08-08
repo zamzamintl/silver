f5addd537b05492119cfc545874772a2da2804b9
Mohamed <mohamed.abdalrman>
2020-08-08 19:32:12 +0200

    [create] slaes team

--- addons/sale_team_custom/__manifest__.py
+++ addons/sale_team_custom/__manifest__.py
@@ -20,12 +20,14 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sales_team','sale'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
+         'security/user_group.xml',
+
+
+
     ],
 
 }
--- addons/sale_team_custom/models/team.py
+++ addons/sale_team_custom/models/team.py
@@ -5,14 +5,26 @@ _logger = logging.getLogger(__name__)
 from odoo import api, fields ,models
 from odoo.exceptions import ValidationError
 from odoo.http import request
+
 class salesteam(models.Model):
-    _inherit = 'crm.teamsorder'
-    count_memeber = fields.Integer(compute='_get_members')
-    @api.depends("member_ids")
-    def _get_members(self):
-        for rec in self.member_ids:
-            if self.user_id and rec.id not in self.user_id.members:
-                self.user_id.members=[(4,rec.id)]
+    _inherit = 'crm.team'
+    @api.constrains("member_ids","user_id")
+    def get_members(self):
+        for team in self:
+            team.favorite_user_ids = [(4, member.id) for member in team.member_ids]
+            team.user_id.members = [(4, member.id) for member in team.member_ids]
+        users = self.env["res.users"].search([])
+        for rec in self.user_id.members:
+            if rec.sale_team_id.user_id.id != self.user_id.id:
+                self.user_id.members=[(3,rec.id)]
+
+
+
+
+
+
+
 class users(models.Model):
     _inherit = 'res.users'
     members = fields.Many2many("res.users","member","id",string="Memebers")
+

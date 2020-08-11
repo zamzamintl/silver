2079f68a897e9e7b7e08742f38e3b8cade9a5fec
Mohamed <mohamed.abdalrman>
2020-08-11 23:00:40 +0200

    Update

--- addons/sales_custom/models/saleorder.py
+++ addons/sales_custom/models/saleorder.py
@@ -5,6 +5,16 @@ _logger = logging.getLogger(__name__)
 from odoo import api, fields ,models
 from odoo.exceptions import ValidationError 
 from odoo.http import request
+from datetime import datetime, timedelta
+from functools import partial
+from itertools import groupby
+
+from odoo import api, fields, models, SUPERUSER_ID, _
+from odoo.exceptions import AccessError, UserError, ValidationError
+from odoo.tools.misc import formatLang, get_lang
+from odoo.osv import expression
+from odoo.tools import float_is_zero, float_compare
+
 
 class order(models.Model):
     _inherit='sale.order'

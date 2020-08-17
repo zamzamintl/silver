788064f0bd203e0de9e324c48f73aa3e69814b8a
Mohamed <mohamed.abdalrman>
2020-08-17 10:42:32 +0200

    [update] activity reprot

--- addons/activity_message-report/__manifest__.py
+++ addons/activity_message-report/__manifest__.py
@@ -11,16 +11,12 @@
     """,
 
     'author': "Mohamed abd elrhamn",
-    'website': "http://www.yourcompany.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'reports',
+    'category': 'mail',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','front_office_management','mail'],
 
     # always loaded
     'data': [
@@ -28,8 +24,5 @@
         'views/views.xml',
         #'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
 }

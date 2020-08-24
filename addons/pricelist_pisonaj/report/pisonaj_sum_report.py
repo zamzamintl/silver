0ecc2524ad5797f4df4b8df66552ac6313bcf726
Mohamed <mohamed.abdalrman>
2020-08-24 23:43:14 +0200

    [update pisonij pricelist ]

--- addons/pricelist_pisonaj/report/pisonaj_sum_report.py
+++ addons/pricelist_pisonaj/report/pisonaj_sum_report.py
@@ -20,29 +20,59 @@ class ReportProductSale(models.AbstractModel):
         i,j=1,1
         pages=[]
         check=False
-        pages.append(j)
+
+        product_cate=[]
         for rec in pricelis.item_ids:
+            if rec.product_tmpl_id.categ_id not in product_cate and rec.product_tmpl_id.categ_id :
+                product_cate.append(rec.product_tmpl_id.categ_id)
 
-            if len(pricelis.item_ids)<=30:
-                docs.append(
-                    {'page': j, 'product_tmpl_id': rec.product_tmpl_id.name, 'fixed_price': rec.fixed_price})
-            if i<=60:
-                if i % 2 == 0:
-                    docs_right.append(
-                        {'page': j, 'product_tmpl_id': rec.product_tmpl_id.name, 'fixed_price': rec.fixed_price})
-                else:
-                    docs_left.append(
-                        {'page': j, 'product_tmpl_id': rec.product_tmpl_id.name, 'fixed_price': rec.fixed_price})
+        cate_id=[]
+        pages.append(j)
+        for record in product_cate:
+            i=0
 
+            count=0
+            for lst in pricelis.item_ids:
+                if record.id == lst.product_tmpl_id.categ_id.id:
+                    count+=1
+
+            print("&&&&&&&&&&&",count)
+            if count<=30:
+                cate_id.append({'page': j, 'cat': record,'check':False})
             else:
-                j += 1
-                pages.append(j)
-                i = 0
+                cate_id.append({'page': j, 'cat': record, 'check': True})
 
-            i+=1
+            for rec in pricelis.item_ids:
+                if record.id == rec.product_tmpl_id.categ_id.id:
+                    if count<=30:
+                        docs.append(
+                            {'page': j,'pro_id':rec.product_tmpl_id,'categ_id':rec.product_tmpl_id.categ_id, 'product_tmpl_id': rec.product_tmpl_id.name, 'fixed_price': rec.fixed_price})
+                    else:
+                         if i<=60:
+                            if j not in pages:
+                                pages.append(j)
+                            if i % 2 == 0:
+                                docs_right.append(
+                                    {'page': j, 'pro_id':rec.product_tmpl_id,'categ_id':rec.product_tmpl_id.categ_id,'product_tmpl_id': rec.product_tmpl_id.name, 'fixed_price': rec.fixed_price})
+                            else:
+                                docs_left.append(
+                                    {'page': j,'pro_id':rec.product_tmpl_id, 'categ_id':rec.product_tmpl_id.categ_id,'product_tmpl_id': rec.product_tmpl_id.name, 'fixed_price': rec.fixed_price})
+
+                         else:
+                            j += 1
+                            pages.append(j)
+                            i = 0
+                            cate_id.append({'page': j, 'cat': record,'check': True})
+
+                i+=1
+            j+=1
+
+
+        print("++++++++++++++++++++++++++++++++***********",cate_id)
         print(docs)
         print(pages)
-        print(len(pricelis.item_ids))
+        print(docs_left)
+        print(docs_right)
         height_field=1
         height=[]
         if len(pricelis.item_ids) >= 30:
@@ -59,5 +89,7 @@ class ReportProductSale(models.AbstractModel):
             'height_field':height_field,
             'check':check,
             'pages':pages,
+            'product_cate':product_cate,
+            'cate_id':cate_id,
             'proforma': True
         }

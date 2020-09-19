8212de1fc4d160a36c118ad46f74a751963a8079
mohamed.abdalrhman <mohamed.abdalrhman0@gmail.com>
2020-09-20 01:17:06 +0200

    create lead and tickets from task and puechsen  person

--- addons/person_purchase/models/models.py
+++ addons/person_purchase/models/models.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 
 class address_book(models.Model):
     _name="person.purchase"
-    product_id = fields.Many2one("product.product",string="Product")
+    product_id = fields.Many2one("product.product",string="Product",domain="[('type_pro','=','vegetables and fruits']")
     purchase_price = fields.Float("Purchase Price")
     @api.constrains("product_id","purchase_price")
     def save_last_purchase(self):

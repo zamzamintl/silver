b06b4c2a7e7c64255d352068ffe4f20a006a3199
mohamed.abdalrhman <mohamed.abdalrhman0@gmail.com>
2020-10-04 21:42:49 +0200

    updates

--- addons/competitor_user/__manifest__.py
+++ addons/competitor_user/__manifest__.py
@@ -12,7 +12,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale'],
+    'depends': ['base','sale','ecomm_custom'],
 
     # always loaded
     'data': [

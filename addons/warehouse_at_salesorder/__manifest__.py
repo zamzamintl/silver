5e080de8e267ea0f9a228998aff3c6fdba057ff5
mohamed.abdalrhman <mohamed.abdalrhman0@gmail.com>
2020-10-12 00:40:36 +0200

    updates at purchase order from so

--- addons/warehouse_at_salesorder/__manifest__.py
+++ addons/warehouse_at_salesorder/__manifest__.py
@@ -5,7 +5,7 @@
     'category': 'sale',
     'description': """create purchase order according to some of sales order
 """,
-    'depends': ['sale'],
+    'depends': ['sale','base','stock'],
     'data': ["views/sales_order_view.xml","security/ir.model.access.csv"],
     'installable': True,
     'auto_install': True,
--- addons/warehouse_at_salesorder/models/partners.py
+++ addons/warehouse_at_salesorder/models/partners.py
@@ -1,12 +1,6 @@
-import logging
-from odoo import fields, http, tools, _
-from odoo.http import request
-_logger = logging.getLogger(__name__)
+
 from odoo import api, fields ,models
-from odoo.exceptions import ValidationError
-from odoo.http import request
-from collections import OrderedDict
-from datetime import datetime
+
 class partner(models.Model):
     _inherit = 'res.partner'
     default_purchase_oer = fields.Boolean(default=False,string="Default vendor")

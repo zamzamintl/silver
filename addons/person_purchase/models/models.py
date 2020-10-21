1d32b21d5d439927347f3223abd2af72c6866435
mohamed.abdalrhman <mohamed.abdalrhman0@gmail.com>
2020-10-21 23:40:35 +0200

    update person purchase no deplicate

--- addons/person_purchase/models/models.py
+++ addons/person_purchase/models/models.py
@@ -7,7 +7,7 @@ class person_purchase(models.Model):
     name =  fields.Char("Person Purchas",default="PR")
     categ_id = fields.Many2one("product.public.category",string="Category")
     person_lines = fields.One2many("person.purchase.line","person_purchase_id",string="Lines")
-    products = fields.Many2many("product.product", "pro", 'id', compute='get_list_products')
+    products = fields.Many2many("product.product", "pro", 'id', compute='get_list_products',store=True)
 
     @api.depends("person_lines")
     def get_list_products(self):
@@ -16,12 +16,12 @@ class person_purchase(models.Model):
             pro = rec.product_id.id
             if pro:
                 self.products = [(4, pro)]
-                rec.products = [(4, pro)]
+
 
 class address_book(models.Model):
     _name="person.purchase.line"
     person_purchase_id = fields.Many2one("person.purchase")
-    products = fields.Many2many("product.product", "pro", 'id')
+    products = fields.Many2many(related='person_purchase_id.products')
     categ_id = fields.Many2one(related='person_purchase_id.categ_id', string="Category")
     product_id = fields.Many2one("product.product",string="Product" ,)
     purchase_price = fields.Float("Purchase Price")
@@ -34,25 +34,28 @@ class address_book(models.Model):
 
             ids=[]
 
-            for rec in self:
+
+            for rec in self.products:
                 # if rec.public_categ_ids == self.categ_id.id and rec.type_pro=='vegetables and fruits':
-                    if rec.product_id:
-                       ids.append(rec._origin.product_id.id)
+
+                       ids.append(rec._origin.id)
             domain=[]
-
-
-
-            if self.categ_id:
-                return {
-                    'domain': {'product_id': [('type_pro','=','vegetables and fruits'),('public_categ_ids','=',self.categ_id.id)]}
-                }
-
+            print("ids",ids)
             if ids:
-                raise ValidationError( ids)
+                print('d')
                 return {
-                    'domain': {'product_id': [('id','not in',ids),('type_pro', '=', 'vegetables and fruits'),
+                    'domain': {'product_id': [('id','not in',ids),
                                               ('public_categ_ids', '=', self.categ_id.id)]}
                 }
+            # ('type_pro', '=', 'vegetables and fruits'),
+            if self.categ_id:
+                print(self.categ_id)
+                print(self._origin.categ_id.name)
+                return {
+                    'domain': {'product_id': [('public_categ_ids','=',self.categ_id.id)]}
+                }
+
+
 
 
     @api.constrains("product_id","purchase_price")

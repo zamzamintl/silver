37018a713df573c181e59c6b3e8194dff4c55f42
mohamed.abdalrhman <mohamed.abdalrhman0@gmail.com>
2020-10-22 10:19:39 +0200

    update person purchase no deplicate

--- addons/competitor_user/models/models.py
+++ addons/competitor_user/models/models.py
@@ -8,14 +8,15 @@ class person_competitor(models.Model):
     creation_date = fields.Date("Creation Date", default=lambda self: fields.datetime.now().date())
     competitor_id = fields.Many2one("res.competitor",string="competitor")
     person_lines = fields.One2many("person.competitor.line","person_competitor_id",string="Lines")
-    products = fields.Many2many("product.product", "pro", 'id', compute='get_list_products')
+    product_ids = fields.Many2many("product.product", "pro", 'id', compute='get_list_product_ids',store=True)
 
     @api.depends("person_lines")
-    def get_list_products(self):
+    def get_list_product_ids(self):
         for rec in self.person_lines:
             pro = rec.product_id.id
             if pro:
-                self.products = [(4, pro)]
+                self.product_ids = [(4, pro)]
+
 
     @api.depends("competitor_id")
     def get_name(self):
@@ -25,22 +26,22 @@ class address_book(models.Model):
     _name="person.competitor.line"
     person_competitor_id = fields.Many2one("person.competitor")
     categ_id = fields.Many2one("product.category",string="Category ")
-    products = fields.Many2many(related='person_competitor_id.products')
+    product_ids = fields.Many2many(related='person_competitor_id.product_ids')
     product_id = fields.Many2one("product.product",string="Product",auto_join=True)
     published = fields.Boolean(related='product_id.is_published',string="publish")
     competitor_price = fields.Float("competitor Price")
     my_price = fields.Float(related='product_id.lst_price')
     creation_date = fields.Date(related='person_competitor_id.creation_date',string="Creation Date")
-    product_id = fields.Many2one("product.product", string="Product", )
+
     # product_ids = fields.Many2many("product.product", string="Product",compute='_get_domain')
     #
     #
     @api.onchange('product_id')
     def get_domain(self):
-        if self.products:
+        if self.product_ids:
             ids = []
 
-            for rec in self.products:
+            for rec in self.product_ids:
                 ids.append(rec._origin.id)
             domain=[]
             if self.categ_id:

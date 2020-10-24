4f2b5c323fc7578f1a334bf24f550827eeb335a8
mohamed.abdalrhman <mohamed.abdalrhman0@gmail.com>
2020-10-25 01:34:52 +0200

    update competitior user

--- addons/competitor_user/reports/competitor_report.py
+++ addons/competitor_user/reports/competitor_report.py
@@ -40,12 +40,13 @@ class ReportProductSale(models.AbstractModel):
         pro_list = []
         price_lst = self.env['product.pricelist.item'].search([('pricelist_id', '=', price_list)])
         print("sss", price_lst)
-        raise ValidationError(price_lst)
+
         for comp_id in lines:
+            raise ValidationError(comp)
 
 
             for plt in price_lst:
-                raise ValidationError(str(comp_id.product_id.product_tmpl_id.id)+str(plt.product_id.id))
+
                 if plt.product_id.id==comp_id.product_id.id and plt.product_id:
 
                     lst.append(
